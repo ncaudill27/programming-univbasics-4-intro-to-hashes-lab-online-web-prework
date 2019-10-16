1d07e46413fc4d58022410d01636160c67877cdb
Nelson A Caudill <nelson.a.caudill@gmail.com>
2019-10-16 21:55:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,11 +21,11 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-  init_hash = Hash[ key, value]
+  init_hash = Hash[key, value]
 end
 
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
+  hash[key]
 end
 
 def update_counting_hash(hash, key)
